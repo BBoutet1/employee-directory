3c427b883e1724ba34a5e2a20d7972e6aef3ac11
Brice Boutet <boutetlb@gmail.com>
2020-06-21 01:42:00 -0400

    Index.css and filter select tag added

--- src/components/EmployeesTable/index.js
+++ src/components/EmployeesTable/index.js
@@ -1,22 +1,28 @@
 import React from "react";
+import "./index.css";
 
 function employeesTable(props) {
     return (
         <main>
-            <div className="">
-                <div className="input-group m-3 w-25 ">
-                    <div className="input-group-prepend">
+            <div className="m-3">
+                <form className="search d-inline-block input-group m-3">
+                    <div className="input-group">
+                        <input type="text" className="form-control" placeholder="Search"/>
+                    </div>
+                </form>
+                <div className="filter d-inline-block input-group m-3">
+                    <div className="d-inline-block input-group-prepend">
                         <label className="input-group-text">Filter by...</label>
                     </div>
-                    <select className="custom-select" id="filter">
+                    <select className="custom-select d-inline-block" id="filter">
                         <option value="name">Name</option>
                         <option value="role">role</option>
                         <option value="department">role</option>
                     </select>
                 </div>
             </div>
-            <div className="m-3 border">
-                <table className="table mx-auto  mb-0">
+            <div className="m-3">
+                <table className="table mx-auto  mb-0  border">
                     <thead  className="thead-light">
                         <tr>
                             <th><strong> ID</strong></th>
