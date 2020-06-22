a9b8f0b352cac83c73020274d31e653380be5272
Brice Boutet <boutetlb@gmail.com>
2020-06-22 17:36:48 -0400

    Updates

---
+++ precache-manifest.20494e2a35ba90edbfd1637c7d8b3d32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5db0cf87d4ad005b1dcbec2fcef0aca",
+    "url": "/BBoutet1/employee-directory/index.html"
+  },
+  {
+    "revision": "210886a0ef4f1874a863",
+    "url": "/BBoutet1/employee-directory/static/css/main.a515b075.chunk.css"
+  },
+  {
+    "revision": "7a0dd8cb4ad07d11a24a",
+    "url": "/BBoutet1/employee-directory/static/js/2.1939b703.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BBoutet1/employee-directory/static/js/2.1939b703.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "210886a0ef4f1874a863",
+    "url": "/BBoutet1/employee-directory/static/js/main.7d9b136b.chunk.js"
+  },
+  {
+    "revision": "9bfe6cd6c5d795a3e56c",
+    "url": "/BBoutet1/employee-directory/static/js/runtime-main.05b53c73.js"
+  }
+]);
